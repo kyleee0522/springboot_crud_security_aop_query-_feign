3b82020565c6103b60729ba8fcc1971b23eeba34
kyleee0522 <kyleee0522@gmail.com>
2024-03-14 13:11:37 +0900

    to macbook

--- src/main/java/com/kt/edu/thirdproject/common/config/SecurityConfig.java
+++ src/main/java/com/kt/edu/thirdproject/common/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
                                 .requestMatchers(
                                         "/",
                                         "/actuator/**",
-                                        "/v3/api-docs/**",
+                                            "/v3/api-docs/**",
                                         "/swagger-ui.html",
                                         "/swagger-ui/**",
                                         "/api/login",
